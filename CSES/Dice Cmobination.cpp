fb0e6ec50fc4f9931cc3d5fe534a98a626c45dda
Lavish Meena <lavish6036@gmail.com>
2026-08-08 16:16:02 +0530

    update

---
+++ CSES/Dice Cmobination.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+
+int mod = 1e9 + 7;
+int ans = 0;
+
+bool solve(int n){
+    if(n == 0){
+        ans += 1;
+        return true;;
+    }
+
+    for(int i = 0; i<n; i++){
+        if(n - i >= 0){
+            ans = 1 + 
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    
+    solve(n);
+
+    cout << ans;
+
+    return 0;
+}
